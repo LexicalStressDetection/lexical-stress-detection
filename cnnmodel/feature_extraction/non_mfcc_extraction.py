b2a3cdb2160a58466daffb1a061b1b77f55beb75
Vivek Kumar <vivekuma@uw.edu>
2020-02-06 21:22:59 -0800

    change train test split to a class

--- cnnmodel/feature_extraction/non_mfcc_extraction.py
+++ cnnmodel/feature_extraction/non_mfcc_extraction.py
@@ -1,5 +1,5 @@
 import numpy
-from . import mfcc_extraction
+from cnnmodel.feature_extraction import mfcc_extraction
 
 EPS = 1e-8  # 0.00000001
 win_length = 0.025
--- cnnmodel/feature_extraction/train_test_split.py
+++ cnnmodel/feature_extraction/train_test_split.py
@@ -1,54 +1,75 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 import numpy as np
-
-ROOT_DATA_PATH = "/Users/maddy/Downloads/Capstone/DATA_FOLDER"
-train_dir = "/Users/maddy/Downloads/Capstone/data_train"
-test_dir = "/Users/maddy/Downloads/Capstone/data_test"
+import multiprocessing as mp
 
 
-def assert_out_dir_exists(root, index):
-    dir_ = root + '/' + str(index)
-    if not os.path.exists(dir_):
-        os.makedirs(dir_)
-        print('crated dir {}'.format(dir_))
-    else:
-        print('dir {} already exists'.format(dir_))
-    return dir_
+class TrainTestSplit:
+    def __init__(self, data_path, train_path, test_path, test_size):
+        self.data_path = data_path
+        self.train_path = train_path + '/train_data'
+        self.test_path = test_path + '/test_data'
+        self.test_size = test_size
+
+        self.pool = mp.Pool(mp.cpu_count())
+        self.make_dirs()
+
+    def make_dirs(self):
+        os.makedirs(self.train_path, exist_ok=True)
+        os.makedirs(self.test_path, exist_ok=True)
+
+    @staticmethod
+    def assert_out_dir_exists(root, label):
+        dir_ = root + '/' + str(label)
+        if not os.path.exists(dir_):
+            os.makedirs(dir_)
+            print('crated dir {}'.format(dir_))
+        else:
+            print('dir {} already exists'.format(dir_))
+        return dir_
+
+    @staticmethod
+    def copy_file(src, dest):
+        shutil.copy(src=src, dst=dest)
+        print('copied file {} to {}'.format(src, dest))
+
+    def copy_npy_files(self, file, dest_root, label):
+        src_mfcc = file
+        src_non_mfcc = src_mfcc.replace('mfcc', 'other')
+
+        dest_mfcc = dest_root + '/' + label + '/' + src_mfcc.split('/')[-1]
+        dest_non_mfcc = dest_mfcc.replace('mfcc', 'other')
+
+        self.pool.apply_async(TrainTestSplit.copy_file, args=[src_mfcc, dest_mfcc])
+        self.pool.apply_async(TrainTestSplit.copy_file, args=[src_non_mfcc, dest_non_mfcc])
+
+    def train_test_split(self):
+        for label in os.listdir(self.data_path):
+            if not label.startswith('.'):
+                files_iter = Path(self.data_path + '/' + label).glob('*_mfcc.npy')
+                files_ = [str(f) for f in files_iter]
+                files_ = np.array(files_)
+                TrainTestSplit.assert_out_dir_exists(self.train_path, label)
+                TrainTestSplit.assert_out_dir_exists(self.test_path, label)
+                choices = np.random.choice([0, 1], size=files_.shape[0], p=(1 - self.test_size, self.test_size))
+                train_files = files_[choices == 0]
+                test_files = files_[choices == 1]
+
+                for train_sample in train_files:
+                    self.copy_npy_files(train_sample, self.train_path, label)
+                for test_sample in test_files:
+                    self.copy_npy_files(test_sample, self.test_path, label)
+
+                print('submitted all for label: {}'.format(label))
 
 
-def train_test_split(data_path, test_size):
-    os.makedirs(train_dir)
-    os.makedirs(test_dir)
-    for label in os.listdir(data_path):
-        if not label.startswith('.'):
-            files_iter = Path(data_path + '/' + label).glob('*_mfcc.npy')
-            files_ = [str(f) for f in files_iter]
-            files_ = np.array(files_)
-            assert_out_dir_exists(train_dir, label)
-            assert_out_dir_exists(test_dir, label)
-            choices = np.random.choice([0, 1], size=files_.shape[0], p=(1 - test_size, test_size))
-            train_files = files_[choices == 0]
-            test_files = files_[choices == 1]
+def main(data_path, train_path, test_path, test_size):
+    train_test_split = TrainTestSplit(data_path=data_path, train_path=train_path,
+                                      test_path=test_path, test_size=test_size)
+    train_test_split.train_test_split()
 
-            for train_sample in train_files:
-                non_mfcc_train_sample = '/'.join(train_sample.split('/')[:-1]) + '/' + '_'.join(
-                    train_sample.split('/')[-1].split("_")[:-1]) + "_other.npy"
-                dest = train_dir + '/' + label + '/' + train_sample.split('/')[-1]
-                print('copying file {} to {}'.format(train_sample, dest))
-                shutil.copyfile(train_sample, dest)
-                dest = train_dir + '/' + label + '/' + non_mfcc_train_sample.split('/')[-1]
-                print('copying file {} to {}'.format(non_mfcc_train_sample, dest))
-                shutil.copyfile(non_mfcc_train_sample, dest)
-            for test_sample in test_files:
-                src = test_sample
-                non_mfcc_test_sample = '/'.join(test_sample.split('/')[:-1]) + '/' + '_'.join(
-                    test_sample.split('/')[-1].split("_")[:-1]) + "_other.npy"
-                dest = test_dir + '/' + label + '/' + test_sample.split('/')[-1]
-                print('copying file {} to {}'.format(src, dest))
-                shutil.copyfile(test_sample, test_dir + '/' + label + '/' + test_sample.split('/')[-1])
-                dest = test_dir + '/' + label + '/' + non_mfcc_test_sample.split('/')[-1]
-                print('copying file {} to {}'.format(non_mfcc_test_sample, dest))
-                shutil.copyfile(non_mfcc_test_sample, dest)
-            print('done for label: {}'.format(label))
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], float(sys.argv[4]))
