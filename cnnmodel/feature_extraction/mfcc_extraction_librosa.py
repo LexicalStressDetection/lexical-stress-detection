177eb411c49cf8e30032ff4652a30bb3153eeafc
Aditi <aditisinghal340@gmail.com>
2020-03-07 17:08:24 +0000

    disbale padding while genrating mfcc

--- cnnmodel/feature_extraction/mfcc_extraction_librosa.py
+++ cnnmodel/feature_extraction/mfcc_extraction_librosa.py
@@ -7,7 +7,7 @@ def get_mfcc(signal, samplerate):
     # duration. window_length is set by n_fft and step is set by hop_length
     frame_length = int(0.025 * samplerate)
     step_size = int(0.01 * samplerate)
-    mfcc = librosa.feature.mfcc(signal, samplerate, n_mfcc=13, n_fft=frame_length, hop_length=step_size)
+    mfcc = librosa.feature.mfcc(signal, samplerate, n_mfcc=13, n_fft=frame_length, hop_length=step_size, center=False)
     mfcc_derivative = librosa.feature.delta(mfcc, order=1)
     mfcc_second_derivative = librosa.feature.delta(mfcc, order=2)
 
